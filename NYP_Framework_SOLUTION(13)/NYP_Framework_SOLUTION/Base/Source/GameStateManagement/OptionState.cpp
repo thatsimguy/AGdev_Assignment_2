0ed0a2676560b38b6663fd93aff872f152b52b8e
LAPTOP-UIJUH701\danci <dancidcop@gmail.com>
2018-02-09 01:37:25 +0800

    Options, not used at the moment

---
+++ NYP_Framework_SOLUTION(13)/NYP_Framework_SOLUTION/Base/Source/GameStateManagement/OptionState.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+#include "OptionState.h"
+#include "GL\glew.h"
+#include "../Application.h"
+#include "LoadTGA.h"
+#include "GraphicsManager.h"
+#include "MeshBuilder.h"
+#include "../TextEntity.h"
+#include "RenderHelper.h"
+#include "../SpriteEntity.h"
+#include "../EntityManager.h"
+
+#include "KeyboardController.h"
+#include "SceneManager.h"
+
+COptionState::COptionState()
+{
+}
+
+COptionState::~COptionState()
+{
+}
+
+void COptionState::Init()
+{
+}
+
+void COptionState::Update(double dt)
+{
+}
+
+void COptionState::Render()
+{
+}
+
+void COptionState::Exit()
+{
+}
---
+++ NYP_Framework_SOLUTION(13)/NYP_Framework_SOLUTION/Base/Source/GameStateManagement/OptionState.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Scene.h"
+#include "Mtx44.h"
+#include "Mesh.h"
+#include "MatrixStack.h"
+#include "../FPSCamera.h"
+#include "../SpriteEntity.h"
+
+class SceneManager;
+//class TextEntity;
+class COptionState : public Scene
+{
+public:
+	COptionState();
+	~COptionState();
+
+	virtual void Init();
+	virtual void Update(double dt);
+	virtual void Render();
+	virtual void Exit();
+
+private:
+	FPSCamera camera;
+	SpriteEntity* IntroStateBackground;
+	//	TextEntity* textObj[3];
+};
